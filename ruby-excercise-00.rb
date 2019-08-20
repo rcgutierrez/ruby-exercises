986917932e1bdf22f91ca88419b560480e82a653
Raul Gutierrez <rcgutierreziii@gmail.com>
2019-08-20 18:37:43 -0500

    hello world, print and put commands

---
+++ ruby-excercise-00.rb
@@ -0,0 +1,13 @@
+#print Prints things out on the same line
+print "hello world"
+
+#puts prints things out on a new line
+puts ""
+puts "raul"
+
+puts ""
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
