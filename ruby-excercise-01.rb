77203ebd10f4c54a7fd87b7d2867a8a74908cc86
Raul Gutierrez <rcgutierreziii@gmail.com>
2019-08-20 19:52:09 -0500

    variables

---
+++ ruby-excercise-01.rb
@@ -0,0 +1,20 @@
+# Separate variable name with underscores     !!!
+character_name = "John"
+character_age = "35"
+
+# puts "There once was a man named George"
+# puts "he was 70 years old."
+# puts "He liked the name George"
+# puts "but didn't like being 70."
+
+# Parentheses for string and variable puts     !!!
+# puts ("There once was a man named " + character_name)
+# puts ("he was " + character_age + " years old.")
+# puts ("He liked the name " + character_name)
+# puts ("but didn't like being " + character_age)
+
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+character_name = "Tom"
+puts ("He liked the name " + character_name)
+puts ("but didn't like being " + character_age)
