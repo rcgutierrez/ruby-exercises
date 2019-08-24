b2328c855d30169d31d8e390626a6f1f408fe081
Raul Gutierrez <rcgutierreziii@gmail.com>
2019-08-23 19:53:20 -0500

    mad libs lesson

---
+++ 07-ruby-mad-libs.rb
@@ -0,0 +1,17 @@
+# Building mad libs
+
+# Get answers from user
+puts "Enter a color:"
+color = gets.chomp
+
+puts "Enter a plural_noun:"
+plural_noun = gets.chomp
+
+puts "Enter a celebrity:"
+celebrity = gets.chomp
+
+# Outputs mad lib
+puts
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
