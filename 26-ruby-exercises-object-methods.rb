0406726b1429144176063d27ebaf9e31c1e3da36
Raul Gutierrez <rcgutierreziii@gmail.com>
2019-09-10 23:19:02 -0500

    object methods lesson

---
+++ 26-ruby-exercises-object-methods.rb
@@ -0,0 +1,29 @@
+# Object methods/Instance methods
+
+class Student
+  attr_accessor :name, :major, :gpa
+  def initialize(name, major, gpa)
+    @name = name
+    @major = major
+    @gpa = gpa
+  end
+  # this method will be accessible to each instance of this class
+  def has_honors
+    if @gpa >= 3.5
+    return true
+    end
+    return false
+  end
+
+end
+
+student1 = Student.new("Jim", "Business", 2.6)
+student2 = Student.new("Pam", "Art", 3.6)
+
+# will return true or false depending on each instance of a class
+puts student1.has_honors
+puts student2.has_honors
+
+# Outputs
+# false
+# true
